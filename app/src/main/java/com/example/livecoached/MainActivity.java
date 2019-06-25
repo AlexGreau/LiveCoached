3e02feb80c8c94c9b45ba27ec9120ff28d5c5253
AlexG <alexgreau@live.fr>
2019-06-25 17:06:31 +0800

    getting list of sensors

--- app/src/main/java/com/example/livecoached/MainActivity.java
+++ app/src/main/java/com/example/livecoached/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.livecoached;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        init();
+        setAmbientEnabled();
+    }
 
+    public void init(){
+        setContentView(R.layout.activity_main);
+        initSensors();
+        initTestText();
+    }
+
+    private void initTestText() {
         mTextView = (TextView) findViewById(R.id.text);
 
         // Enables Always-on
-        setAmbientEnabled();
+    }
+
+    private void initSensors() {
+        System.out.println("printing sensors");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        System.out.println(sensors);
     }
 }
