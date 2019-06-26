6f856c5bc3fa9eb744b9e337bd2128f7d3303d9a
AlexG <alexgreau@live.fr>
2019-06-26 11:50:39 +0800

    sending button

--- app/src/main/java/com/example/livecoached/MainActivity.java
+++ app/src/main/java/com/example/livecoached/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.livecoached;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.provider.ContactsContract;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,9 +32,36 @@ public class MainActivity extends WearableActivity {
         initSensors();
         initTestText();
         initButtons();
+        initConnection();
     }
 
     private void initButtons() {
+        initVibrateButton();
+        initSendingButton();
+    }
+
+    private void initTestText() {
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+
+    private void initSensors() {
+        System.out.println("printing sensors");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        System.out.println(sensors);
+    }
+
+    public void initSendingButton(){
+        ImageButton sendButton = (ImageButton) findViewById(R.id.button_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSensors();
+            }
+        });
+    }
+
+    public void initVibrateButton(){
         Button vibrateButton = (Button) findViewById(R.id.button_vibrate);
         vibrateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,17 +71,12 @@ public class MainActivity extends WearableActivity {
         });
     }
 
-    private void initTestText() {
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
+    public void initConnection(){
+        System.out.println("initiating connection");
     }
 
-    private void initSensors() {
-        System.out.println("printing sensors");
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        System.out.println(sensors);
+    public void sendSensors(){
+        System.out.println("sending sensors");
     }
 
     public void vibrate(){
