e711a3820f22af696f1b8417fed87c1790b7f747
AlexG <alexgreau@live.fr>
2019-06-26 11:18:16 +0800

    vibrating watch

--- app/src/main/java/com/example/livecoached/MainActivity.java
+++ app/src/main/java/com/example/livecoached/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class MainActivity extends WearableActivity {
     }
 
     private void initButtons() {
-
+        Button vibrateButton = (Button) findViewById(R.id.button_vibrate);
+        vibrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrate();
+            }
+        });
     }
 
     private void initTestText() {
@@ -43,5 +52,14 @@ public class MainActivity extends WearableActivity {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
         System.out.println(sensors);
+    }
+
+    public void vibrate(){
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long[] vibrationPattern = {0, 500, 50, 300};
+        //-1 - don't repeat
+        final int indexInPatternToRepeat = -1;
+        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+        System.out.println("vibrating");
     }
 }
