2e1f8df8706b4d4644801bfc7cd6222abfb01478
AlexG <alexgreau@live.fr>
2019-07-23 12:08:52 +0800

    comparison function for critical points

--- app/src/main/java/com/example/livecoached/Model/CriticalPoint.java
+++ app/src/main/java/com/example/livecoached/Model/CriticalPoint.java
@@ -3,10 +3,22 @@ package com.example.livecoached.Model;
 public class CriticalPoint {
     private float latitude;
     private float longitude;
+    private double bearing;
 
-    public CriticalPoint(String lat, String lng){
+    public CriticalPoint(String lat, String lng) {
         this.latitude = Float.parseFloat(lat);
         this.longitude = Float.parseFloat(lng);
+        this.bearing = 0.0;
+    }
+
+    public CriticalPoint(String lat, String lng, String bear) {
+        this.latitude = Float.parseFloat(lat);
+        this.longitude = Float.parseFloat(lng);
+        this.bearing = Double.parseDouble(bear);
+    }
+
+    public double getBearing() {
+        return bearing;
     }
 
     public float getLatitude() {
@@ -15,5 +27,14 @@ public class CriticalPoint {
 
     public float getLongitude() {
         return longitude;
+    }
+
+    public boolean matches(CriticalPoint dest, float errorMargin) {
+        if (dest.getLatitude() <= (this.latitude + errorMargin) || dest.getLatitude() >= (this.latitude - errorMargin)) {
+            if (dest.getLongitude() <= (this.longitude + errorMargin) || dest.getLongitude() >= (this.longitude - errorMargin)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
